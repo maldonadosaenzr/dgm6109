7c07908b0f8654823e80685dcc64c10682851ebd
maldonadosaenzr <maldonadosaenz.r@northeastern.edu>
2025-10-27 19:18:35 -0700

    Add files via upload

---
+++ term:phase_2/main.js
@@ -0,0 +1,69 @@
+
+
+
+
+
+let nights= [{
+date:20-oct-2025, 
+coffees_Morning: 3,
+coffees_Afternoon: 0,
+no_light_Time_: false
+bedtime_duration: 5,
+quality_Sleep:
+}, //end "night" object #1
+
+
+{date:21-oct-2025, 
+coffees_Morning: 1,
+coffees_Afternoon: 2,
+no_light_Time_: true
+bedtime_duration: 5,
+quality_Sleep:
+}, //end "night" object #2
+
+
+{date:22-oct-2025, 
+coffees_Morning: 3,
+coffees_Afternoon: 1,
+no_light_Time_: true
+bedtime_duration: 8,
+quality_Sleep:
+}, //end "night" object #3
+
+
+{date:23-oct-2025, 
+coffees_Morning: 2,
+coffees_Afternoon: 0,
+no_light_Time_: true
+bedtime_duration: 8,
+quality_Sleep:
+}, //end "night" object #4
+
+
+{date:24-oct-2025, 
+coffees_Morning: 0,
+coffees_Afternoon: 0,
+no_light_Time_: false
+bedtime_duration: 6,
+quality_Sleep:
+}, //end "night" object #5
+
+
+{date:25-oct-2025, 
+coffees_Morning: 0,
+coffees_Afternoon: 1,
+no_light_Time_: true
+bedtime_duration: 5,
+quality_Sleep:
+}, //end "night" object #6
+
+
+{date:26-oct-2025, 
+coffees_Morning: 2,
+coffees_Afternoon: 1,
+no_light_Time_: false
+bedtime_duration: 5,
+quality_Sleep:
+}, //end "night" object #7
+
+]
