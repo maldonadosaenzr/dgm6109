4f8555b9adf758bd59f96296c4c0e74d81815626
maldonadosaenzr <maldonadosaenz.r@northeastern.edu>
2025-11-30 16:59:22 -0800

    Add files via upload

---
+++ proyect2/main.js
@@ -0,0 +1,8 @@
+function validateData () {
+
+   if {("charicard"(value <= 1000 imput, 0))}
+
+}
+
+// tried multiple options and sadly didnt worked//
+// also flowchart button seems to not work even when found no error//
